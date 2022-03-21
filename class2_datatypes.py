e0bd1a4129946d8fbfee05aa9b207eb705b88202
satzcloud <satishk_87@yahoo.co.in>
2022-03-21 20:51:03 +0530

    initial commit

---
+++ class2_datatypes.py
@@ -0,0 +1,9 @@
+# Datatypes are of several Types, Some are below
+# 1. integer 2. Float 3. Boolean etc.
+
+# To find the type of datatype. Use type command
+
+print(type('hello'))
+print(type(1))
+print(type(1.64))
+print(type(True))
