c1e129f27efe827544160375771298df22a11bf0
satzcloud <satishk_87@yahoo.co.in>
2022-03-21 21:01:39 +0530

    integers, strings and floats "to" conversion

--- class2_datatypes.py
+++ class2_datatypes.py
@@ -7,3 +7,27 @@ print(type('hello'))
 print(type(1))
 print(type(1.64))
 print(type(True))
+
+# To convert an integer to string
+# sample code
+failed_subjects = 2
+name = 'john'
+print('Your Son ' + name + ' is failing ' + str(failed_subjects) + ' subjects.')
+
+
+# Lets Print Integers and see the output
+a = int(1)
+b = int(2.5)
+c = int("3")
+c1 = int(float("3.4")) # To make string to work on int, We need to convert to float
+d = float(1)
+e = float(2.5)
+f = float("3")
+g = float("4.23")
+h = str("80s")
+i = str(22)
+j = str(3.01)
+
+# Print as array of variables hence we are using []
+print ([a,b,c,c1,d,e,f,g,h,i,j])
+
